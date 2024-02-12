dd157e64549f1b7278dc3b53d851da95db729b0c
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2024-02-12 22:50:36 +0100

    new: added bootstraped serviceVersioningEntities at startup

--- src/main/java/com/oneshoot/versiontrackerservice/VersionTrackerServiceApplication.java
+++ src/main/java/com/oneshoot/versiontrackerservice/VersionTrackerServiceApplication.java
@@ -1,13 +1,35 @@
 package com.oneshoot.versiontrackerservice;
 
+import com.oneshoot.versiontrackerservice.entities.ServiceVersioningEntity;
+import com.oneshoot.versiontrackerservice.repositories.ServicesVersioningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class VersionTrackerServiceApplication {
+
+    @Autowired
+    ServicesVersioningRepository servicesVersioningRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(VersionTrackerServiceApplication.class, args);
     }
 
+
+    @Bean
+    CommandLineRunner runner(){
+        return args ->{
+            List<ServiceVersioningEntity> versioningEntities = List.of(
+                    new ServiceVersioningEntity(1L,"oneshootmain","1.1.0","1.0.0", LocalDateTime.now()),
+                    new ServiceVersioningEntity(2L,"resumeParser","1.2.0","1.1.0", LocalDateTime.now())
+            );
+            servicesVersioningRepository.saveAll(versioningEntities);
+        };
+    }
 }
